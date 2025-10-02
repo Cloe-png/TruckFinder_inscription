b175e580e9d44434e76ce0c4c07974113568a0b3
Cloe-png <cloewlodarskizminka@gmail.com>
2025-10-02 16:28:40 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,10 +1,11 @@
 <?php
 // Configuration de la base de données
 define('DB_HOST', 'localhost');
-define('DB_NAME', 'foodtruck_app');
+define('DB_NAME', 'truckfinder');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 
+// Connexion PDO
 try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8", DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
