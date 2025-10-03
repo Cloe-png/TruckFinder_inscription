9b9cd9fbd649eabde969ba2d47702418f9aa4389
Cloe-png <cloewlodarskizminka@gmail.com>
2025-10-03 08:49:40 +0200

    Create admin.php

---
+++ views/admin.php
@@ -0,0 +1,16 @@
+<div class="card">
+    <h2 style="color: var(--turquoise);">Demandes en attente</h2>
+    <table>
+        <thead>
+            <tr>
+                <th>Food Truck</th>
+                <th>Gérant</th>
+                <th>Email</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!--{DEMANDES}-->
+        </tbody>
+    </table>
+</div>
